8f6b562a44473ca56b923f75ea265c39846ac902
kengo0629 <63942635+kengo0629@users.noreply.github.com>
2020-05-27 09:49:25 +0900

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,14 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  def edit
+  @product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+    Product.update(product_params)
+    redirect_to action: :index
+  end
 end

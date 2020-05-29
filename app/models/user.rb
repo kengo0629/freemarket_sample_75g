b1cbb2c58b2195380abbcc40750ac54132a79124
gouta11116 <gouta1116@gmail.com>
2020-05-29 12:50:27 +0900

    commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :nickname,:first_name,:last_name,:first_name_kana,:last_name_kana, presence: true
+  validates :email, presence: true, uniqueness:true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 7 }, format: {with: /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./ }
+  has_one :address
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: "users/sessions",
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+    root 'users/registrations#new'
+  end
 end

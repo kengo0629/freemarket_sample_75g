5cb3cc1ef2b21867c811932c67ec7bb7eca72f20
gouta11116 <gouta1116@gmail.com>
2020-06-01 20:42:46 +0900

    commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,8 +10,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AKIAS6CEVI3FITFZWVV3'],
-      aws_secret_access_key: ENV['17cc6357a92773937861cf9eae666aa9'],
+      aws_access_key_id: Rails.application.credentials.aws['AKIAS6CEVI3FITFZWVV3'],
+      aws_secret_access_key: Rails.application.credentials.aws['17cc6357a92773937861cf9eae666aa9'],
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'furima75g'

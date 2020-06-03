5ee8b748ac295c9abbfab9a6eabb467da056f7a9
gouta11116 <gouta1116@gmail.com>
2020-06-03 15:52:28 +0900

    commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
-  else
+  elsif Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {

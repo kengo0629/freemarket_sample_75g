fcc1613ad8055420a0c2f3727f03fa87c5ed51a2
gouta11116 <gouta1116@gmail.com>
2020-06-03 20:07:00 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_121311) do
+ActiveRecord::Schema.define(version: 2020_05_28_080944) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 2020_06_02_121311) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_id"
   end
 
   create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

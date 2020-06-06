9d9891398fa1802b4b2ec1672af97e9a6dc6b8f9
nanbu391 <nabe48665@gmail.com>
2020-06-06 19:30:26 +0900

    commit

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,9 +2,4 @@ class PurchasesController < ApplicationController
   def index
     @product = Product.find(params[:product_id])
   end
-
-  def  done
-    @product= Product.find(params[:id])
-    @product.update( buy_user_id: current_user.id)
-   end
 end
